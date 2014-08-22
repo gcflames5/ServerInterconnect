831d7d2b9d3b241a4d1d00d279aa01b008a73347
gcflames5 <gcflames5@gmail.com>
2014-08-22 18:40:51 -0400

    Add packet resposnes

--- src/main/java/net/njay/serverinterconnect/api/packet/Packet.java
+++ src/main/java/net/njay/serverinterconnect/api/packet/Packet.java
@@ -1,5 +1,6 @@
 package net.njay.serverinterconnect.api.packet;
 
+import com.sun.istack.internal.Nullable;
 import net.njay.serverinterconnect.api.transferable.Transferable;
 
 import java.util.UUID;
@@ -9,11 +10,33 @@ public abstract class Packet implements Transferable {
     public static final int MAX_STRING_SIZE = 32767;
 
     private UUID packetUuid;
+    @Nullable
+    private UUID responseTo;
 
     public Packet(){
-        packetUuid = UUID.randomUUID();
+        this.responseTo = null;
+        this.packetUuid = UUID.randomUUID();
     }
 
     public UUID getPacketUUID(){ return packetUuid; }
+
+    public Packet setResponse(UUID responseToUUID){
+        this.responseTo = responseToUUID;
+        return this;
+    }
+
+    public Packet setResponseTo(Packet responseTo){
+        this.responseTo = responseTo.getPacketUUID();
+        return this;
+    }
+
+    @Nullable
+    public UUID getResponseTo(){
+        return responseTo;
+    }
+
+    public boolean isResponse(){
+        return responseTo != null;
+    }
 
 }
