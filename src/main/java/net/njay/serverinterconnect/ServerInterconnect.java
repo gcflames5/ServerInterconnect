c18ec936c80e37c1b701da34c11183e2ea3cc1b9
gcflames5 <gcflames5@gmail.com>
2014-08-23 12:15:43 -0400

    Add registerCorePackets method

---
+++ src/main/java/net/njay/serverinterconnect/ServerInterconnect.java
@@ -0,0 +1,21 @@
+package net.njay.serverinterconnect;
+
+import net.njay.serverinterconnect.packet.PacketRegistry;
+import net.njay.serverinterconnect.packet.auth.AccessRequestPacket;
+import net.njay.serverinterconnect.packet.auth.AuthenticationRequestPacket;
+import net.njay.serverinterconnect.packet.message.JsonMessagePacket;
+import net.njay.serverinterconnect.packet.message.MessagePacket;
+import net.njay.serverinterconnect.packet.reject.RejectionPacket;
+
+public class ServerInterconnect {
+
+    public static void registerCorePackets() {
+        PacketRegistry.registerPacket(AccessRequestPacket.class);
+        PacketRegistry.registerPacket(AuthenticationRequestPacket.class);
+        PacketRegistry.registerPacket(RejectionPacket.class);
+
+        PacketRegistry.registerPacket(MessagePacket.class);
+        PacketRegistry.registerPacket(JsonMessagePacket.class);
+    }
+
+}
