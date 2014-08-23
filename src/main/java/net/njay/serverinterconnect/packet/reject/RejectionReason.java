ce07f89384c4c07db9f4e944699f8137f4256d08
gcflames5 <gcflames5@gmail.com>
2014-08-23 00:43:10 -0400

    Send RejectionPacket if connection is not authorized

---
+++ src/main/java/net/njay/serverinterconnect/packet/reject/RejectionReason.java
@@ -0,0 +1,7 @@
+package net.njay.serverinterconnect.packet.reject;
+
+public enum RejectionReason {
+
+    CONNECTION_NOT_AUTHORIZED, MALFORMED_PACKET, UNEXPECTED_PACKET;
+
+}
--- src/main/java/net/njay/serverinterconnect/single/server/auth/AuthenticatedTcpReadThread.java
+++ src/main/java/net/njay/serverinterconnect/single/server/auth/AuthenticatedTcpReadThread.java
@@ -8,6 +8,8 @@ import net.njay.serverinterconnect.connection.TcpReadThread;
 import net.njay.serverinterconnect.event.PacketRecievedEvent;
 import net.njay.serverinterconnect.packet.auth.AccessRequestPacket;
 import net.njay.serverinterconnect.packet.auth.AuthenticationRequestPacket;
+import net.njay.serverinterconnect.packet.reject.RejectionPacket;
+import net.njay.serverinterconnect.packet.reject.RejectionReason;
 import net.njay.serverinterconnect.utils.packet.PacketUtils;
 
 import java.io.IOException;
@@ -30,8 +32,11 @@ public class AuthenticatedTcpReadThread extends TcpReadThread {
                 if (manager.isAuthenticated(conn))
                     Event.callEvent(new PacketRecievedEvent(conn, p));
                 else{
+                    System.out.println("PACKET: " + p.getClass() + " " + p + " " + (p instanceof AuthenticationPacket));
                     if (p instanceof AccessRequestPacket || p instanceof AuthenticationPacket || p instanceof AuthenticationRequestPacket)
                         Event.callEvent(new PacketRecievedEvent(conn, p));
+                    else
+                        conn.sendPacket(new RejectionPacket(p, RejectionReason.CONNECTION_NOT_AUTHORIZED).setResponse(p));
                 }
             } catch (SocketTimeoutException e){
                 conn.terminate();
