80985e42d1fa07b39d44a7d2876a2420295ba90b
gcflames5 <gcflames5@gmail.com>
2014-08-22 21:21:45 -0400

    Add per packet resposnes

--- src/main/java/net/njay/serverinterconnect/packet/reponse/AutoResponder.java
+++ src/main/java/net/njay/serverinterconnect/packet/reponse/AutoResponder.java
@@ -8,30 +8,52 @@ import net.njay.serverinterconnect.event.PacketRecievedEvent;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class AutoResponder implements Listener {
 
-    private Map<Class<? extends Packet>, Response> waitingResponses = new HashMap<Class<? extends Packet>, Response>();
+    private Map<Class<? extends Packet>, Response> pendingResponses = new HashMap<Class<? extends Packet>, Response>();
+    private Map<UUID, Response> pendingPacketResponses = new HashMap<UUID, Response>();
 
     public AutoResponder(){
         Event.addListener(this);
     }
 
     public void respondTo(Class<? extends Packet> toRespond, Response response){
-        waitingResponses.put(toRespond, response);
+        pendingResponses.put(toRespond, response);
     }
 
-    public Map<Class<? extends Packet>, Response> getWaitingResponses(){
-        return waitingResponses;
+    public void onResponse(Packet packet, Response response){
+        pendingPacketResponses.put(packet.getPacketUUID(), response);
     }
+
+    public Map<Class<? extends Packet>, Response> getPendingResponses(){
+        return pendingResponses;
+    }
+
+    public Map<UUID, Response> getPendingPacketResponses(){
+        return pendingPacketResponses;
+    }
+
 
     @EventHandler
     public void onRecieve(PacketRecievedEvent e){
-        if (!waitingResponses.containsKey(e.getPacket().getClass())) return;
-        Response response = waitingResponses.get(e.getPacket().getClass());
+        if (!pendingResponses.containsKey(e.getPacket().getClass())) return;
+        Response response = pendingResponses.get(e.getPacket().getClass());
         response.event = e;
         response.packet = e.getPacket();
         response.run();
+    }
+
+    @EventHandler
+    public void onPacketRecieve(PacketRecievedEvent e){
+        if (!e.getPacket().isResponse()) return;
+        if (!pendingPacketResponses.containsKey(e.getPacket().getResponseTo())) return;
+        Response response = pendingPacketResponses.get(e.getPacket().getResponseTo());
+        response.event = e;
+        response.packet = e.getPacket();
+        response.run();
+        pendingPacketResponses.remove(e.getPacket().getPacketUUID());
     }
 
 }
